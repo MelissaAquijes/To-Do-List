331171f270655837e96813df9b91d5f755276cf4
Melissa Aquijes <melissa.geral10@gmail.com>
2023-11-04 23:53:57 -0500

    Se crea componente header y archivo css para estilos del header.

---
+++ src/AppUI.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Header } from './components/Header';
+
+function AppUI() {
+  return(
+      <Header />
+
+  );
+}
+
+export { AppUI };
---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+import profileImg from '../assets/profile.jpg';
+import '../styles/Header.css';
+
+function Header() {
+  return(
+    <header>
+      <img
+        className='profileImg'
+        src={profileImg}
+        alt='User profile' />
+
+      <div className='welcomeContainer'>
+        <p className='welcome'>
+          Welcome!
+        </p>
+
+        <h2 className='userName'>
+          Melissa Aquijes
+        </h2>
+      </div>
+    </header>
+  );
+}
+
+export { Header };
