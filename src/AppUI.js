1f83c01c88e3db349c63cdcf8c0f150e016c4461
Melissa Aquijes <melissa.geral10@gmail.com>
2023-11-06 12:20:24 -0500

    Se agrega componente para contador de tareas realizadas.

--- src/AppUI.js
+++ src/AppUI.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import { Header } from './components/Header';
 import { ContainerAll } from './ContainerAll';
+import { ContainerNav } from './ContainerNav';
 import { TodoSearch } from './components/TodoSearch';
+import { CreateTodoButton } from './components/CreateTodoButton';
+import { TodoTittleCounter } from './components/TodoTittleCounter';
+
 
 function AppUI() {
   return(
     <ContainerAll>
       <Header />
-      <TodoSearch />
+      <ContainerNav>
+        <TodoSearch />
+        <CreateTodoButton />
+      </ContainerNav>
+      <TodoTittleCounter />
+
     </ContainerAll>
 
   );
---
+++ src/components/TodoTittleCounter.jsx
@@ -0,0 +1,9 @@
+function TodoTittleCounter() {
+  return (
+    <h1>
+      Has completado 2 de 5 to do's.
+    </h1>
+  );
+}
+
+export { TodoTittleCounter };
