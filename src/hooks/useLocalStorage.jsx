1bf18bb2a1a76f439e67e83cd283f43db0e505c5
Melissa Aquijes <melissa.geral10@gmail.com>
2023-11-17 12:40:00 -0500

    Modificación del array.

--- src/hooks/useLocalStorage.jsx
+++ src/hooks/useLocalStorage.jsx
@@ -26,7 +26,7 @@ function useLocalStorage(itemName, initialValue) {
         setError(true);
       }
     }, 2000);
-  }, []);
+  }, [itemName, initialValue]);
 
   const saveItem = (newItem) => {
     localStorage.setItem(itemName, JSON.stringify(newItem));
